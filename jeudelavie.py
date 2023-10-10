428095b7fa1b88067e82991fdfce0dc1fd81cd34
Florian Margage <florian.margage@hotmail.com>
2023-10-10 11:32:25 +0200

    edit readme

--- jeudelavie.py
+++ jeudelavie.py
@@ -5,25 +5,6 @@ from matplotlib.colors import ListedColormap
 from matplotlib.backend_bases import MouseButton
 import json as js
 
-'''
-Touches :
-Augmenter d'une étape: Z ou flèche du droite
-Descendre d'une étape: A ou flèche de gauche
-Augmenter la vitesse: + ou flèche du haut
-Diminuer la vitesse: - ou flèche du bas
-Augmenter les naissances : B
-Diminuer les naissances : N
-
-Enregistrer: E
-Charger: R
-
-Mettre en pause: P
-
-Fonctionnalités:
-Inverser les cellules vivantes et mortes : ESPACE
-Ajouter une cellule vivante: Clic Gauche
-Ajouter un motif étoile: Clic Droit
-'''
 
 def save():
     global grid
